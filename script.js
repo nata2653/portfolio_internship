cac3cf9720a030afee8f03d2e92a149fe1d24018
Natalie Sirich <nata2653@stud.kea.dk>
2019-10-23 21:54:46 +0200

    add burgermenu and some stuff

--- script.js
+++ script.js
@@ -1,7 +1,14 @@
 "use strict";
 
-window.onscroll = () => {
-  const nav = document.querySelector("#nav-bar");
-  if (this.scrollY <= 50) nav.className = "";
-  else nav.className = "nav-colored";
-};
+var burgerMenu = document.getElementById("burger-menu");
+var overlay = document.getElementById("menu");
+burgerMenu.addEventListener("click", function() {
+  this.classList.toggle("close");
+  overlay.classList.toggle("overlay");
+});
+
+// window.onscroll = () => {
+//   const nav = document.querySelector("#nav-bar");
+//   if (this.scrollX <= 50) nav.className = "";
+//   else nav.className = "nav-colored";
+// };
