5cf2d39041ae071c92db1517bf82cb1bcced9985
Natalie Sirich <nata2653@stud.kea.dk>
2019-10-24 11:52:00 +0200

    add responsiv 1.0

--- script.js
+++ script.js
@@ -1,11 +1,34 @@
 "use strict";
 
-var burgerMenu = document.getElementById("burger-menu");
-var overlay = document.getElementById("menu");
+document.addEventListener("scroll", navColor);
+document.addEventListener("scroll", burgerColor);
+
+const burgerMenu = document.querySelector("#burger-menu");
+const overlay = document.querySelector("#menu");
 burgerMenu.addEventListener("click", function() {
   this.classList.toggle("close");
   overlay.classList.toggle("overlay");
 });
+
+function navColor() {
+  const scrollY = window.pageYOffset;
+  const nav = document.querySelector("#nav-bar");
+
+  console.log(scrollY);
+
+  scrollY >= 742 ? nav.classList.add("nav-colored") : "";
+  scrollY < 742 ? nav.classList.remove("nav-colored") : "";
+}
+
+function burgerColor() {
+  const scrollY = window.pageYOffset;
+  const burger = document.querySelector("#burger-menu>span");
+
+  console.log(scrollY);
+
+  scrollY >= 1047 ? burger.classList.add("burger-background-dark") : "";
+  scrollY < 1047 ? burger.classList.remove("burger-background-dark") : "";
+}
 
 // window.onscroll = () => {
 //   const nav = document.querySelector("#nav-bar");
